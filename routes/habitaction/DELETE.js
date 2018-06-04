fb0d37a8ff770ba150ac7488d621f74d9b778f0d
immacmillan <immacmillan@gmail.com>
2018-06-04 19:28:26 -0400

    fixed delete route

--- routes/habitaction/DELETE.js
+++ routes/habitaction/DELETE.js
@@ -10,17 +10,21 @@ module.exports = function deleteHabit(req, res, next) {
 
 	return Habit.remove({_id: req.params.id})
 				.then(() => {
-					res.sendStatus(CONST.HTTP_STATUS_CODE.OK);
+					res.send({success: true, message: CONST.HTTP_STATUS_CODE.OK});
 				}).catch((err) => {
-					return next(err);
+					return next({
+						status: CONST.HTTP_STATUS_CODE.BAD_REQUEST,
+						error:{
+							success:false,
+							message:err
+						},
+					});
 				});
 			
 };
 
 /** Currently when you delete a habit, it gets deleted from the Habit Model, but not the User sub-document. In this way it is possible to retain history of user "attempted or previously added habits"
-
 If we want to remove this behavior - this is a possible method using .pull to remove sub-documents (results in ref error): 
-
 return user.userhabits.pull({_id: req.params._id}, function (err, user) {
 	if (err) console.log(err);
 	user.save();
@@ -31,5 +35,4 @@ return user.userhabits.pull({_id: req.params._id}, function (err, user) {
 }).catch((err) => {
 	return next(err);
 });
-
  */
